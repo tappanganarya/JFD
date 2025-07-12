c1e4494eb284dde326a29dddceb57c3975799e5a
tappanganarya <rynaldyk@mail.com>
2025-07-12 11:40:35 +0800

    genap ganjil

---
+++ looping.js
@@ -0,0 +1,26 @@
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+// }
+
+
+// let temp = '';
+
+// for (let i = 0; i < 3; i++) {
+//     for (let j = 0; j < 4; j++) {
+//         temp += 'x';
+//     }
+//     console.log(temp);
+//     temp = '';
+// }
+
+// Study case 1
+
+let num = 10;
+
+for (let i = 0; i <= num; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " Genap");
+    } else {
+        console.log(i + " Ganjil");
+    }
+}
