e5b027f4dd8df871ad3566770af3a8a7b2f25d61
tappanganarya <rynaldyk@mail.com>
2025-07-12 14:16:06 +0800

    array

---
+++ Array.js
@@ -0,0 +1,6 @@
+let scores = ["A", "B", "C"];
+
+
+for (let i = 0; i < scores.length; i++) {
+    console.log(i + 1 + ". " + scores[i]);
+}
--- looping.js
+++ looping.js
@@ -27,19 +27,59 @@
 
 // study case 2 
 
-let number = 15
-let factor = 0
-let bf = 0
+// let number = 15
+// let factor = 0
+// let bf = 0
 
-for (let i = 1; i <= number; i++) {
-    if (number % i === 0) {
-        console.log("Faktor " + number + " adalah " + i);
-        factor++;
-    } else {
-        console.log("Nilai " + i + " Bukan Faktor ");
-        bf++;
+// for (let i = 1; i <= number; i++) {
+//     if (number % i === 0) {
+//         console.log("Faktor " + number + " adalah " + i);
+//         factor++;
+//     } else {
+//         console.log("Nilai " + i + " Bukan Faktor ");
+//         bf++;
+//     }
+// }
+// console.log("Factor = " + factor);
+// console.log("Bukan Factor = " + bf);
+
+// study case 3
+
+// let number = 7
+// let factor = 0
+
+
+// for (let i = 1; i <= number; i++) {
+//     if (number % i === 0) {
+//         factor++;
+//     }
+// }
+
+// if (factor === 2) {
+//     console.log(number + " bilangan prima");
+// } else {
+//     console.log(number + " bukan bilanga prima")
+// }
+
+// study case 4
+
+// let number = 5;
+// let factor = 1;
+
+
+// for (let i = number; i >= 1; i--) {
+//     factor = factor * i;
+// }
+
+// console.log(factor);
+
+let num = 5;
+let temp = ''
+
+for (let i = 0; i < num; i++) {
+    for (let j = 0; j <= i; j++) {
+        temp = temp + 'x';
     }
+    console.log(temp);
+    temp = ''
 }
-console.log("Factor = " + factor);
-console.log("Bukan Factor = " + bf);
-
