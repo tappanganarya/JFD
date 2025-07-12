9e301d338de281a6d3c45081b18194b6ad2b614b
tappanganarya <rynaldyk@mail.com>
2025-07-12 12:01:20 +0800

    factor

--- looping.js
+++ looping.js
@@ -15,12 +15,31 @@
 
 // Study case 1
 
-let num = 10;
+// let num = 10;
 
-for (let i = 0; i <= num; i++) {
-    if (i % 2 === 0) {
-        console.log(i + " Genap");
+// for (let i = 0; i <= num; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i + " Genap");
+//     } else {
+//         console.log(i + " Ganjil");
+//     }
+// }
+
+// study case 2 
+
+let number = 15
+let factor = 0
+let bf = 0
+
+for (let i = 1; i <= number; i++) {
+    if (number % i === 0) {
+        console.log("Faktor " + number + " adalah " + i);
+        factor++;
     } else {
-        console.log(i + " Ganjil");
+        console.log("Nilai " + i + " Bukan Faktor ");
+        bf++;
     }
 }
+console.log("Factor = " + factor);
+console.log("Bukan Factor = " + bf);
+
